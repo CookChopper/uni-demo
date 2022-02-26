b54ba0e2f0dac98f811ebd7e3fa6e58098f7430e
remo <15626475734@163.com>
2022-02-26 23:44:39 +0800

    完成商品列表功能

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,72 @@
+<template>
+  <view class="goods-item" @click="goToGoodsDetail(goods)">
+    <view class="goods-item-left">
+      <image :src="goods.goods_small_logo || defaultPic" mode=""></image>
+    </view>
+    <view class="goods-item-right">
+      <view class="goods-name">
+        {{goods.goods_name}}
+      </view>
+      <view class="goods-price">
+        ¥{{goods.goods_price | tofixed}}
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"my-goods",
+    props: {
+      goods: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+    data() {
+      return {
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    },
+    methods:{
+      goToGoodsDetail(item){
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      }
+    },
+    filters:{
+      tofixed(num){
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.goods-item{
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #efefef;
+    .goods-item-left{
+      margin-right: 10px;
+      image {
+        display: block;
+        width: 120px;
+        height: 120px;
+      }
+    }
+    .goods-item-right{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .goods-name{
+        font-size: 13px;
+      }
+      .goods-price{
+        font-size: 16px;
+        color: #C00000;
+      }
+    }
+  }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    商品列表
+    <view class="goods-list">
+      <my-goods v-for="(goods,i) in goodsList" :key="i" :goods="goods">
+      
+      </my-goods>
+    </view>
   </view>
 </template>
 
@@ -8,12 +12,83 @@
   export default {
     data() {
       return {
-        
+        queryInfo:{
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        total: 0, // 商品列表结果总数
+        goodsList: [], // 商品列表
+        isloading: false, //节流阀
       };
+    },
+    onLoad(options){
+      // console.log(options)
+      this.queryInfo.query = options.query || ''
+      this.queryInfo.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    onPullDownRefresh(){
+      this.goodsList = []
+      this.total = 0
+      this.isloading = false
+      this.queryInfo.pagenum = 1
+      this.getGoodsList(() => {
+        uni.stopPullDownRefresh()
+      })
+    },
+    onReachBottom(){
+      // console.log(1)
+      // if(this.queryInfo.pagesize * this.queryInfo.pagenum >= this.total) return uni.$showMsg('数据加载完毕！')
+      // 目前api接口有问题 当20条数据 只能请求前20条
+      if(this.queryInfo.pagesize * (this.queryInfo.pagenum + 1) >= this.total) return uni.$showMsg('数据加载完毕！！')
+      if(this.isloading) return 
+      this.queryInfo.pagenum += 1
+      this.getGoodsList()
+    },
+    methods:{
+      /**
+       * 获取商品列表
+       */
+      async getGoodsList(cb){
+        this.isloading = true
+        const { data : res } = await uni.$http.get('/api/public/v1/goods/search',this.queryInfo)
+        cb && cb()
+        this.goodsList = [...this.goodsList,...res.message.goods]
+        this.total = res.message.total
+        this.isloading = false
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+.goods-list{
+  .goods-item{
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #efefef;
+    .goods-item-left{
+      margin-right: 10px;
+      image {
+        display: block;
+        width: 120px;
+        height: 120px;
+      }
+    }
+    .goods-item-right{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .goods-name{
+        font-size: 13px;
+      }
+      .goods-price{
+        font-size: 16px;
+        color: #C00000;
+      }
+    }
+  }
+}
 </style>
